f23333792ccfffb725b5e8a71c6e3cca2992a565
edwardzjl <jameszhou2108@hotmail.com>
2019-01-03 14:15:41 +0800

    finish tests dir

--- tests/distributed/test_basic.py
+++ tests/distributed/test_basic.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import xgboost as xgb
 
 # Always call this before using distributed module
--- tests/distributed/test_issue3402.py
+++ tests/distributed/test_issue3402.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import xgboost as xgb
 
 xgb.rabit.init()
